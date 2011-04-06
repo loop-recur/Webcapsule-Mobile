8e5b66f6dff6b3d7eabbe16e272a9b8d8a808a27
Taylor Smith <tjsmith19@gmail.com>
2011-04-06 12:58:09 -0700

    loading screen and landing page

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,48 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-
-//
-// create base UI tab and root window
-//
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
+    title:'Stories',
+    // url:'main_windows/play.js',
     backgroundColor:'#fff'
 });
+
 var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
+    icon:'KS_nav_ui.png',
+    title:'Stories',
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var win2 = Titanium.UI.createWindow({
+	title:'Record',
+    // url:'main_windows/record.js',
+ 	backgroundColor:'#fff'
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
 var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
+    icon:'KS_nav_views.png',
+    title:'Record',
     window:win2
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var win3 = Titanium.UI.createWindow({  
+    title:'Users',
+    // url:'main_windows/play.js',
+    backgroundColor:'#fff'
 });
 
-win2.add(label2);
+var tab3 = Titanium.UI.createTab({  
+    icon:'KS_nav_ui.png',
+    title:'Users',
+    window:win3
+});
 
-
-
-//
-//  add tabs
-//
 tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+tabGroup.addTab(tab2); 
+tabGroup.addTab(tab3);  
 
 
-// open tab group
-tabGroup.open();
+tabGroup.open({
+  transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT 
+});
