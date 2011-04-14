0ea46e41a5917a401b860a5296c3c53170b36009
Taylor Smith <tjsmith19@gmail.com>
2011-04-13 18:21:44 -0700

    comment

--- Resources/layouts/bottom_nav.js
+++ Resources/layouts/bottom_nav.js
@@ -1,40 +1,40 @@
 // BOTTOM TAB BAR
 
-var win = Titanium.UI.currentWindow;
-
-var bottomView = Titanium.UI.createView({
-	bottom:0,
-	height:58,
-	backgroundImage:'images/toolbar/toolbar_bg.png'
-}); 
-
-var storiesTab = Titanium.UI.createButton({
-	value:false,
-	bottom:5,
-	left:30,
-	height:41,
-	width:49,
-	backgroundImage:'images/toolbar/books_ico.png'
-});
-
-var usersTab = Titanium.UI.createButton({
-	value:false,
-	bottom:7,
-	right:23,
-	height:37,
-	width:69,
-	backgroundImage:'images/toolbar/users_ico.png'
-});
-
-var recordTab = Titanium.UI.createButton({
-	value:false,
-	bottom:0,
-	height:69,
-	width:122,
-	backgroundImage:'images/toolbar/newstory_btn.png'
-});
-
-win.add(bottomView);
-win.add(storiesTab);
-win.add(usersTab);
-win.add(recordTab);
+// var win = Titanium.UI.currentWindow;
+// 
+// var bottomView = Titanium.UI.createView({
+// 	bottom:0,
+// 	height:58,
+// 	backgroundImage:'images/toolbar/toolbar_bg.png'
+// }); 
+// 
+// var storiesTab = Titanium.UI.createButton({
+// 	value:false,
+// 	bottom:5,
+// 	left:30,
+// 	height:41,
+// 	width:49,
+// 	backgroundImage:'images/toolbar/books_ico.png'
+// });
+// 
+// var usersTab = Titanium.UI.createButton({
+// 	value:false,
+// 	bottom:7,
+// 	right:23,
+// 	height:37,
+// 	width:69,
+// 	backgroundImage:'images/toolbar/users_ico.png'
+// });
+// 
+// var recordTab = Titanium.UI.createButton({
+// 	value:false,
+// 	bottom:0,
+// 	height:69,
+// 	width:122,
+// 	backgroundImage:'images/toolbar/newstory_btn.png'
+// });
+// 
+// win.add(bottomView);
+// win.add(storiesTab);
+// win.add(usersTab);
+// win.add(recordTab);
