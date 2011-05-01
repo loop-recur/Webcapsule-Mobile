d99d3562048fa3f02c6723ab51b59e41b47d976b
drboolean <brian@lateporter.com>
2011-05-01 11:46:13 -0700

    started tags

---
+++ Resources/controllers/tags_controller.js
@@ -0,0 +1,9 @@
+Controllers.tags = {
+	db: Db("tags"),
+
+	init: function(view, params) {
+		var date = new Date;
+		var tag = {id: "temp-" + Ti.Utils.md5HexDigest(date.toString())};
+		view.render(tag, params);
+	}	
+};
