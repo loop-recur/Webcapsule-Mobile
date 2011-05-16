6180468d80d7648ad5ac50fbabc852010a87a53e
drboolean <brian@lateporter.com>
2011-05-16 15:46:20 -0700

    set iterval experiment

--- Resources/helpers/player.js
+++ Resources/helpers/player.js
@@ -4,6 +4,7 @@ Helpers.player.timeMonitor = function(win, player, comments, photos) {
 	var appear_time = 4;
 	var self = this;
 	var done = false;
+	var stop;
 	
 	player.addEventListener('complete',function(e){
 		done = true;
@@ -11,18 +12,24 @@ Helpers.player.timeMonitor = function(win, player, comments, photos) {
 	});
 	
 	
-	showOverlays();
+	start();
+	
+	function start() {
+		var intervalId = setInterval(showOverlays, 1000);
+		stop = function() {
+			clearInterval(intervalId);
+		};
+	};
 	
 	function hideAllOverlays() {
 		Functional.map(".close", [Views.comments._comment, Views.photos._photo]);
 	};
 	
-	function showOverlays() {
-		if(done){ clearInterval(showOverlays); };
+	function showOverlays(interval) {
+		if(done){ return stop(); };
 		var position = player.currentPlaybackTime;
 		Functional.map(showOverlay.partial(Views.comments._comment, position), comments);
 		Functional.map(showOverlay.partial(Views.photos._photo, position), photos);
-		setInterval(showOverlays, 1000);
 	};
 
 	function showOverlay(view, position, item) {
