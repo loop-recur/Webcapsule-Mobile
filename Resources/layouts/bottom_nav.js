42038f0ffb1cf70dcc82695471cfbed013fc8f8e
Taylor Smith <tjsmith19@gmail.com>
2011-05-21 11:31:42 -0700

    logout confirmation

--- Resources/layouts/bottom_nav.js
+++ Resources/layouts/bottom_nav.js
@@ -71,7 +71,15 @@ Layouts.bottomNav = function() {
 	});
 	
 	logout_button.addEventListener('click', function(){
-		Controllers.user_sessions.destroy();
+		
+		Helpers.ui.confirm("Temporary Logout Button: Logout?", {
+			yes : function() {
+				Controllers.user_sessions.destroy();
+			},
+			cancel : function() {}
+		});
+		
+		
 	});
 
 	win.add(bottom_view);
