0345d74212027476c52ec22e0493fd582f99e6ff
Taylor Smith <tjsmith19@gmail.com>
2011-06-28 16:36:15 -0600

    user button update
    orientation first try

--- Resources/config/environments.js
+++ Resources/config/environments.js
@@ -1,11 +1,11 @@
 App.environments = {
 	development: function() {
 		App.isDevelopment = true;
-		App.file_url = "http://10.0.2.2:3000";
-		App.base_url = "http://10.0.2.2:3000/i_phone";
+		App.file_url = "http://www.webcapsule.com";
+		App.base_url = "http://www.webcapsule.com/i_phone";
 	},
 	production: function() {
-		App.file_url = "http://192.168.1.11:3000";
-		App.base_url = "http://192.168.1.11:3000/i_phone";
+		App.file_url = "http://www.webcapsule.com";
+		App.base_url = "http://www.webcapsule.com/i_phone";
 	}
 };
